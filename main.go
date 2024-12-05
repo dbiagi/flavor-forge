71af1364d5ef91d0f3f90a5850194b07e27f3e69
Diego de Biagi <diegobiagi@protonmail.com>
2024-12-05 16:27:27 -0300

    feat: added startup time

--- main.go
+++ main.go
@@ -1,107 +1,112 @@
 package main
 
 import (
-	"context"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/dbiagi/gororoba/src/config"
-	"github.com/dbiagi/gororoba/src/controller"
-	"github.com/dbiagi/gororoba/src/handler"
-	"github.com/dbiagi/gororoba/src/repository"
-	"github.com/gorilla/mux"
-	"log/slog"
-	"net/http"
-	"os"
-	"os/signal"
+    "context"
+    "fmt"
+    "github.com/aws/aws-sdk-go/service/dynamodb"
+    "github.com/dbiagi/gororoba/src/config"
+    "github.com/dbiagi/gororoba/src/controller"
+    "github.com/dbiagi/gororoba/src/handler"
+    "github.com/dbiagi/gororoba/src/repository"
+    "github.com/gorilla/mux"
+    "log/slog"
+    "net/http"
+    "os"
+    "os/signal"
+    "time"
 )
 
 type Controllers struct {
-	controller.HealthCheckController
-	controller.RecipesController
+    controller.HealthCheckController
+    controller.RecipesController
 }
 
 func main() {
-	appEnv := os.Getenv("APP_ENV")
-	if appEnv == "" {
-		appEnv = "development"
-	}
+    startTime := time.Now()
+    appEnv := os.Getenv("APP_ENV")
+    if appEnv == "" {
+        appEnv = "development"
+    }
 
-	appConfig := config.LoadConfig(appEnv)
+    appConfig := config.LoadConfig(appEnv)
 
-	config.ConfigureLogger(appConfig.AppConfig)
+    config.ConfigureLogger(appConfig.AppConfig)
 
-	slog.Info("Connecting to dynamoDB ....")
-	dynamoDB := connectToDynamoDB(appConfig.AWSConfig)
+    slog.Info("Connecting to dynamoDB ....")
+    dynamoDB := connectToDynamoDB(appConfig.AWSConfig)
 
-	slog.Info("Starting server ....")
-	srv, router := createServer(appConfig.WebConfig)
+    slog.Info("Starting server ....")
+    srv, router := createServer(appConfig.WebConfig)
 
-	slog.Info("Creating resources ....")
-	appResources := createControllers(dynamoDB)
+    slog.Info("Creating resources ....")
+    appResources := createControllers(dynamoDB)
 
-	slog.Info("Registering routes and serving ....")
-	registerRoutesAndServe(router, appResources)
+    slog.Info("Registering routes and serving ....")
+    registerRoutesAndServe(router, appResources)
 
-	slog.Info("Configuring graceful shutdown ....")
-	configureGracefullShutdown(srv, appConfig.WebConfig)
+    slog.Info(fmt.Sprintf("Application ready. Time elapsed: %v", time.Since(startTime)))
+
+    slog.Info("Configuring graceful shutdown.")
+    configureGracefullShutdown(srv, appConfig.WebConfig)
 }
 
 func connectToDynamoDB(awsConfig config.AWSConfig) *dynamodb.DynamoDB {
-	dynamoDB, err := config.CreateDynamoDBConnection(awsConfig)
-	if err != nil {
-		slog.Error("Error connecting to dynamodb.", slog.String("error", err.Message))
-		panic(err)
-	}
+    dynamoDB, err := config.CreateDynamoDBConnection(awsConfig)
+    if err != nil {
+        slog.Error("Error connecting to dynamodb.", slog.String("error", err.Message))
+        panic(err)
+    }
 
-	return dynamoDB
+    return dynamoDB
 }
 
 func createServer(webConfig config.WebConfig) (*http.Server, *mux.Router) {
-	router := mux.NewRouter()
-	srv := &http.Server{
-		Addr:         ":" + os.Getenv("PORT"),
-		Handler:      router,
-		IdleTimeout:  webConfig.IdleTimeout,
-		ReadTimeout:  webConfig.ReadTimeout,
-		WriteTimeout: webConfig.WriteTimeout,
-	}
+    router := mux.NewRouter()
+    srv := &http.Server{
+        Addr:         ":" + os.Getenv("PORT"),
+        Handler:      router,
+        IdleTimeout:  webConfig.IdleTimeout,
+        ReadTimeout:  webConfig.ReadTimeout,
+        WriteTimeout: webConfig.WriteTimeout,
+    }
 
-	go func() {
-		err := srv.ListenAndServe()
-		if err != nil {
-			slog.Error("Error starting server.", slog.String("error", err.Error()))
-		}
-	}()
+    go func() {
+        err := srv.ListenAndServe()
+        if err != nil {
+            slog.Error("Error starting server.", slog.String("error", err.Error()))
+        }
+    }()
 
-	return srv, router
+    return srv, router
 }
 
 func createControllers(db *dynamodb.DynamoDB) Controllers {
-	recipeRepository := repository.NewRecipeRepository(db)
-	healthCheckHandler := handler.NewHealthCheckHandler()
-	recipeHandler := handler.NewRecipesHandler(recipeRepository)
+    recipeRepository := repository.NewRecipeRepository(db)
+    healthCheckHandler := handler.NewHealthCheckHandler()
+    recipeHandler := handler.NewRecipesHandler(recipeRepository)
 
-	return Controllers{
-		RecipesController:     controller.NewRecipesController(recipeHandler),
-		HealthCheckController: controller.NewHealthCheckController(healthCheckHandler),
-	}
+    return Controllers{
+        RecipesController:     controller.NewRecipesController(recipeHandler),
+        HealthCheckController: controller.NewHealthCheckController(healthCheckHandler),
+    }
 }
 
 func registerRoutesAndServe(router *mux.Router, controllers Controllers) {
-	router.Use(config.TraceIdMiddleware)
-	router.HandleFunc("/health", controller.HandleRequest(controllers.HealthCheckController.Check)).Methods("GET")
-	router.HandleFunc("/health/complete", controller.HandleRequest(controllers.HealthCheckController.CheckComplete)).Methods("GET")
-	router.HandleFunc("/recipes", controller.HandleRequest(controllers.RecipesController.GetRecipes)).Methods("GET")
+    router.Use(config.TraceIdMiddleware)
+    router.HandleFunc("/health", controller.HandleRequest(controllers.HealthCheckController.Check)).Methods("GET")
+    router.HandleFunc("/health/complete", controller.HandleRequest(controllers.HealthCheckController.CheckComplete)).Methods("GET")
+    router.HandleFunc("/recipes", controller.HandleRequest(controllers.RecipesController.GetRecipes)).Methods("GET")
 }
 
 func configureGracefullShutdown(server *http.Server, webConfig config.WebConfig) {
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	<-c
+    c := make(chan os.Signal, 1)
+    signal.Notify(c, os.Interrupt)
+    <-c
 
-	ctx, cancel := context.WithTimeout(context.Background(), webConfig.ShutdownTimeout)
-	defer cancel()
+    ctx, cancel := context.WithTimeout(context.Background(), webConfig.ShutdownTimeout)
+    defer cancel()
 
-	server.Shutdown(ctx)
-	slog.Info("Shutting down server")
-	os.Exit(0)
+    server.Shutdown(ctx)
+    slog.Info("Shutting down server")
+    os.Exit(0)
 }
